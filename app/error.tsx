cee80de718529ee10267438e84f45918dff4ab5d
abid-hasan-99 <abid.hasan@findit.id>
2025-06-10 19:49:03 +0600

    updated error for product details

--- app/error.tsx
+++ app/error.tsx
@@ -12,32 +12,30 @@ interface ErrorPageProps {
 export default function ErrorPage({ error, reset }: ErrorPageProps) {
   console.error(error);
   return (
-    <section className="">
-      <Error
-        message={
-          <>
-            <h2 className="text-5xl font-semibold text-red-600 text-center my-8">
-              Something went wrong
-            </h2>
+    <Error
+      message={
+        <>
+          <h2 className="text-5xl font-semibold text-red-600 text-center my-8">
+            Something went wrong
+          </h2>
 
-            <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
-              <Button
-                onClick={reset}
-                className="px-4 py-2 bg-primary text-white rounded-sm hover:opacity-85 transition-opacity"
-              >
-                Try again
-              </Button>
-              <Link
-                href="/products"
-                className="text-2xl text-center text-primary hover:underline"
-              >
-                Navigate to products
-              </Link>
-            </div>
-          </>
-        }
-        containerClassname="text-center"
-      />
-    </section>
+          <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
+            <Button
+              onClick={reset}
+              className="px-4 py-2 bg-primary text-white rounded-sm hover:opacity-85 transition-opacity"
+            >
+              Try again
+            </Button>
+            <Link
+              href="/products"
+              className="text-2xl text-center text-primary hover:underline"
+            >
+              Navigate to products
+            </Link>
+          </div>
+        </>
+      }
+      containerClassname="text-center"
+    />
   );
 }
